852d6eb7fbb747fd78b44e153fc30247fa3720be
Xau <frozzy.xfs@gmail.com>
2023-06-16 04:22:42 -0400

    Add playRound function

--- game.js
+++ game.js
@@ -10,3 +10,25 @@ function getComputerChoice() {
         return "Scissor";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let userInput = playerSelection.toLowerCase();
+    let computerInput = computerSelection.toLowerCase();
+
+    // Tie
+    if (userInput === computerInput) {
+        return "You Tied! Both of you chose " + computerSelection + ".";
+    }
+
+    // Lost
+    else if (userInput === "rock" && computerInput === "paper"
+            || userInput === "paper" && computerInput === "scissor"
+            || userInput === "scissor" && computerInput === "rock") {
+                return "You Lose! " + computerInput + " beats " + userInput + ".";
+            } 
+
+    // Won
+    else {
+        return "You Win! " + userInput + " beats " + computerInput + "."; 
+    }
+}
