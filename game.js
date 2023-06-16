fb86da951e08c4ed74fd963d5223fefea2bbf45b
Xau <frozzy.xfs@gmail.com>
2023-06-16 04:14:09 -0400

    Add getComputerChoice function

---
+++ game.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3);
+    if (rand === 0) {
+        return "Rock";
+    }
+    else if (rand === 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissor";
+    }
+}
